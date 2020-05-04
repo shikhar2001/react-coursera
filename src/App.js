ac55629e9c39a04dee293aca52eb21f41971c800
shikhar2001 <imt_2019095@iiitm.ac.in>
2020-05-04 20:26:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
-import '../src/App.css'
+import '../src/App.css';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
  render() {
   return (
+    <BrowserRouter>
     <div className="App">
-    <Main />
-  </div>
+      <Main />
+    </div>
+  </BrowserRouter>
   )
 }
 }
