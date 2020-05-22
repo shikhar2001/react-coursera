e5da1afed82a08d2e6d7b789ea772a8c2e81b0b3
shikhar2001 <imt_2019095@iiitm.ac.in>
2020-05-23 03:16:53 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,12 +9,20 @@ import { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 function RenderDish({ dish }) {
     if (dish != null)
         return (
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             <Card>
                 <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
@@ -22,6 +30,7 @@ function RenderDish({ dish }) {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
     else
         return (
@@ -31,18 +40,23 @@ function RenderDish({ dish }) {
 }
 function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
-        const dishcomments = comments.map((comment) => {
+        return (
+            <div>
+                <h4>Comments</h4>
+                <div>
+                <Stagger in>
+                {comments.map((comment) => {
             return (
+                    <Fade in>
                 <li key={comment.id} className="list-unstyled">
                     <p>{comment.comment}</p>
                     <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
+                </Fade>
             )
-        })
-        return (
-            <div>
-                <h4>Comments</h4>
-                <div>{dishcomments}</div>
+        })}
+                </Stagger>
+                </div>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
